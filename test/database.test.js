ee62572d334915aa0b84c4a8658ed97c84f34e06
AymanAlQoqa <aalqouqa@gmail.com>
2019-02-27 15:50:24 +0200

    database tests relates#14

--- test/database.test.js
+++ test/database.test.js
@@ -1,6 +1,47 @@
-const tape = require("tape");
+const test = require("tape");
+const runDbBuild = require('../src/database/build');
+const addPost = require('../src/model/addPost');
+const addUser = require('../src/model/addUser');
+const getPost = require('../src/model/getPosts');
+const getUser = require('../src/model/getUser');
 
-tape("Testing tape", t => {
-  t.equal(1, 1, "Pass");
-  t.end();
-});
+test('test getuser from users table', (t) => {
+  runDbBuild();
+  getUser('asd@asd.com').then((result) => {
+    t.equal(typeof result, 'object', 'should return object');
+    t.end();
+  })
+})
+
+test('test getPost from posts table', (t) => {
+  runDbBuild();
+  getPost().then((result) => {
+    t.equal(result.rows.length, 2, 'should return 2');
+    t.end();
+  })
+})
+
+test('test addPost from posts table', (t) => {
+  runDbBuild();
+  addPost('addfdsffdth',1).then(() => {
+    getPost().then((result) => {
+      t.equal(result.rows.length, 3, 'should return 3');
+      t.end();
+    })
+  })
+})
+
+test('test addUser from users table', (t) => {
+  runDbBuild();
+  addUser('asffd', 'resddff', 'asdd@asfd.com', '$2a$05$4ZI8rNffE53KoLw40vBYKzcO1CUKICCKO7LkEfbQ.Gjk0mNKV20Rycu').then(() => {
+    getUser('asdd@asfd.com').then((result) => {
+      t.equal(result.rows[0].email, 'asdd@asfd.com', 'should return the same email');
+      t.end();
+    })
+  })
+})
+
+
+test.onFinish(() => {
+  process.exit(0);
+})
