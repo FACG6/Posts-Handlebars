dd3f269b9e283c9f6fcb59ba47c4ce8f2ffdf6bb
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-02-27 15:28:31 +0200

    update src controllers

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,10 +1,15 @@
 const getPosts = require("./../model/getPosts.js");
+const { verify } = require("jsonwebtoken");
 exports.get = (req, res, next) => {
-  getPosts()
-    .then(result => {
-      res.render("home", { posts: result.rows, css: "profile" });
-    })
-    .catch(err => {
-      next(err);
-    });
+  verify(req.cookies.jwt, process.env.SECRET, (err, token) => {
+    if (err) res.redirect("/signin");
+    else
+      getPosts()
+        .then(result => {
+          res.render("home", { posts: result.rows, css: "profile", js: "profile" });
+        })
+        .catch(err => {
+          next(err);
+        });
+  });
 };
