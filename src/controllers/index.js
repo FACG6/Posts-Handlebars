c948feab196a44ac1128fbb53898ca742cb45964
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-02-27 11:55:37 +0200

    conflicts

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const error = require('./error.js');
-const home = require('./home.js');
-const profile = require('./profile.js');
-const signin = require('./signin.js');
-const signup = require('./signup.js');
-const addPost = require('./addPost.js');
+const error = require("./error.js");
+const home = require("./home.js");
+const profile = require("./profile.js");
+const signin = require("./signin.js");
+const signup = require("./signup.js");
+const addPost = require("./addPost.js");
 
-router.get('/', home.get);
-router.get('/profile', profile.get);
-router.post('/signup', signup.post);
-router.post('/signin', signin.post);
-router.post('/add-post', addPost.post);
+router.get("/", home.get);
+router.get("/profile", profile.get);
+router.get("/signup", signup.get);
+router.post("/signup", signup.post);
+router.post("/signin", signin.post);
+router.post("/add-post", addPost.post);
 
 router.use(error.client);
 router.use(error.server);
--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,29 +1,44 @@
-const bcrypt = require('bcryptjs');
-const error = require('./error.js')
-const addUser = require('./../model/addUser.js');
-const getUser = require('../model/getUser')
+const bcrypt = require("bcryptjs");
+const error = require("./error.js");
+const addUser = require("./../model/addUser.js");
+const getUser = require("../model/getUser");
+
+exports.get = (request, response) => {
+  response.render("signup", { js: "signup", css: "signup" });
+};
+
 exports.post = (request, response) => {
-    const userData = request.body;
-    getUser(userData.email).then(result => {
-        if (result.length === 0) {
-            bcrypt.genSalt(5).then(salt => {
-                bcrypt.hash(userData.password, salt).then(pass => {
-
-                    addUser(userData.firstname, userData.lastname, userData.email, pass).then(() => {
-                        response.redirect('/signin');
-                    }).catch(err => {
-                        error.server(err, request, response, null);
-                    })
-                }).catch(err => {
-                    error.server(err, request, response, null);
+  const userData = request.body;
+  getUser(userData.email).then(result => {
+    if (result.length === 0) {
+      bcrypt
+        .genSalt(5)
+        .then(salt => {
+          bcrypt
+            .hash(userData.password, salt)
+            .then(pass => {
+              addUser(
+                userData.firstname,
+                userData.lastname,
+                userData.email,
+                pass
+              )
+                .then(() => {
+                  response.redirect("/signin");
                 })
-            }).catch(err => {
-                error.server(err, request, response, null);
+                .catch(err => {
+                  error.server(err, request, response, null);
+                });
             })
-        } else {
-            response.render('signin', { msg: "user is already exist " })
-        }
-
-    })
-
-}
+            .catch(err => {
+              error.server(err, request, response, null);
+            });
+        })
+        .catch(err => {
+          error.server(err, request, response, null);
+        });
+    } else {
+      response.render("signin", { msg: "user is already exist " });
+    }
+  });
+};
